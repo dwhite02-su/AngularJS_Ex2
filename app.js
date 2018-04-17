2e53272f6cb1b68a19d7ca20e9de9272c26d12fc
jonbackus <jonbackus@users.noreply.github.com>
2018-04-17 19:38:02 -0400

    Starting files

---
+++ app.js
@@ -0,0 +1,42 @@
+var app = angular.module('weather', []);
+
+app.run(function($rootScope){
+    //Your API Key
+    $rootScope.key = '';
+    
+
+    //Searched for ZIP code
+    $rootScope.zip = null;
+
+
+    //Toggle between views
+    $rootScope.displayWeather = false;
+});
+
+//Navbar component
+
+
+
+
+
+//Search component
+
+
+
+
+
+//Curent Weather Widget Component
+
+//Controller for Current Weather Component
+
+
+
+
+
+//Forecast Controller
+
+
+
+
+
+//Forecast Item Component
